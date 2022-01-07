025295d26efe34bfa8346ea7cab787b01c9e6fec
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-01-06 23:34:50 -0500

    added foreign key

--- db/migrate/20220107025954_create_restaurants.rb
+++ db/migrate/20220107025954_create_restaurants.rb
@@ -3,6 +3,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.1]
     create_table :restaurants do |t|
       t.string :name 
       t.string :category
+      t.integer :user_id
   end
 end
 end
